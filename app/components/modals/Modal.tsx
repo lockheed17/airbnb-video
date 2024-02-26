9c1b95af61731d08f3e9573ffc6e7d19af9104f1
lockheed17 <slavyanoserbsk1@ukr.net>
2024-02-26 23:46:47 +0200

    added RegisterModal, useRegisterModal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
     actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
     actionLabel,
     disabled,
     secondaryAction,
-    secondaryLabel
+    secondaryActionLabel
 }) => {
     const [showModal, setShowModal] = useState(isOpen);
 
@@ -42,7 +42,7 @@ const Modal: React.FC<ModalProps> = ({
 
         setShowModal(false);
         setTimeout(() => {
-            onClose;
+            onClose();
         }, 300);
 
     }, [disabled, onClose]);
@@ -168,7 +168,20 @@ const Modal: React.FC<ModalProps> = ({
                                         w-full
                                     "
                                 >
-                                    <Button label="My Button"/>
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            outline
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                        />
+                                    )}
+                                    <Button
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    />
+
                                 </div>
                             </div>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from "@/app/components/navbar/Navbar";
 import ClientOnly from './components/ClientOnly';
-import Modal from "@/app/components/modals/Modal";
+import RegisterModal from "@/app/components/modals/RegisterModal";
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello world" isOpen />
+          <RegisterModal />
           <Navbar  />
         </ClientOnly>
         {/*{children}*/}
